7e2767637092c16b218719452a5f925a844fd237
Nada Mikky <142957229+NadaMikky@users.noreply.github.com>
2025-04-10 14:06:33 -0400

    MySQL local added

---
+++ SQL/MySQL Local.session.sql
@@ -0,0 +1,7 @@
+# create your tables here 
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; 
