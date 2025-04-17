b7b663657b8686f36f7e91c38478811316897b6f
Brittany <bmorgan6@mix.wvu.edu>
2025-04-17 14:10:47 -0400

    Testing Insert Query

--- TakeMeHomeDatabase.session.sql
+++ TakeMeHomeDatabase.session.sql
@@ -1,23 +1,3 @@
-CREATE TABLE Ride_Request (
-    ID_Number INT PRIMARY KEY,
-    Trip_Date DATE NOT NULL,
-    Meet_up_Location VARCHAR(100) NOT NULL,
-    Destination VARCHAR(100) NOT NULL,
-    Passenger_Student_ID VARCHAR(20),
-    Driver_ID VARCHAR(20),
-    FOREIGN KEY (Passenger_Student_ID) REFERENCES Passenger(Student_ID),
-    FOREIGN KEY (Driver_ID) REFERENCES Driver(Student_ID)
-);
-
-CREATE TABLE Ride_Offer (
-    ID_Number INT PRIMARY KEY,
-    Trip_Date DATE NOT NULL,
-    Meet_up_Location VARCHAR(100) NOT NULL,
-    Destination VARCHAR(100) NOT NULL,
-    Meet_up_Time TIME NOT NULL,
-    Driver_ID VARCHAR(20),
-    Passenger_ID VARCHAR(20),
-    FOREIGN KEY (Driver_ID) REFERENCES Driver(Student_ID),
-    FOREIGN KEY (Passenger_ID) REFERENCES Passenger(Student_ID)
-);
+INSERT INTO Users
+VALUES("Jess Remphrey","jer00027@mix.wvu.edu", "password", "800727272", "Sophomore");
 
