f4d03f82d2d22bf83a2fe4de35fdf2645f5ef598
Brittany <bmorgan6@mix.wvu.edu>
2025-04-24 17:21:05 -0400

    updating database stuff... code needs editted

---
+++ Avin.session.sql
@@ -0,0 +1,56 @@
+CREATE TABLE Users (
+    Name VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL,
+    Student_ID VARCHAR(20) PRIMARY KEY,
+    Class VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Passenger (
+    Student_ID VARCHAR(20) PRIMARY KEY,
+    Has_Luggage BOOLEAN,
+    Payment VARCHAR(20) NOT NULL,
+    FOREIGN Key (Student_ID) REFERENCES Users(Student_ID)
+);
+
+CREATE TABLE Driver (
+    Student_ID VARCHAR(20) PRIMARY KEY,
+    License_Number VARCHAR(20) NOT NULL,
+    Allow_Smoking BOOLEAN,
+    Insurance_Company VARCHAR(50) NOT NULL,
+    FOREIGN Key (Student_ID) REFERENCES Users(Student_ID)
+);
+
+CREATE TABLE Vehicle (
+    Make VARCHAR(50) NOT NULL,
+    Model VARCHAR(50) NOT NULL,
+    Year INT NOT NULL,
+    VIN VARCHAR(20) NOT NULL UNIQUE,
+    License_Plate VARCHAR(20) PRIMARY KEY,
+    Seating_Capacity INT NOT NULL,
+    Student_ID VARCHAR(20) ,
+    FOREIGN KEY (Student_ID) REFERENCES Driver(Student_ID)
+);
+
+CREATE TABLE Ride_Request (
+    ID_Number INT PRIMARY KEY,
+    Trip_Date DATE NOT NULL,
+    Meet_up_Location VARCHAR(100) NOT NULL,
+    Destination VARCHAR(100) NOT NULL,
+    Passenger_Student_ID VARCHAR(20),
+    Driver_ID VARCHAR(20),
+    FOREIGN KEY (Passenger_Student_ID) REFERENCES Passenger(Student_ID),
+    FOREIGN KEY (Driver_ID) REFERENCES Driver(Student_ID)
+);
+
+CREATE TABLE Ride_Offer (
+    ID_Number INT PRIMARY KEY,
+    Trip_Date DATE NOT NULL,
+    Meet_up_Location VARCHAR(100) NOT NULL,
+    Destination VARCHAR(100) NOT NULL,
+    Meet_up_Time TIME NOT NULL,
+    Driver_ID VARCHAR(20),
+    Passenger_ID VARCHAR(20)
+    FOREIGN KEY (Driver_ID) REFERENCES Driver(Student_ID),
+    FOREIGN KEY (Passenger_ID) REFERENCES Passenger(Student_ID)
+);
