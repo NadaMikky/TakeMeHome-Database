99895f5c1070f04a3b5adf8c2dcbd40d133b5e9c
Nada Mikky <142957229+NadaMikky@users.noreply.github.com>
2025-04-29 13:36:34 -0400

    importing react to each file

---
+++ frontend/src/createAccount.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+import './createAccount.css'; // Importing CSS for styling
---
+++ frontend/src/listings.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+import './listings.css'; // Importing CSS for styling
