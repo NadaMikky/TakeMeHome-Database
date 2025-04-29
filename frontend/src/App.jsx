55459b68d9d95e57190f0859038277c857dbf1c4
Nada Mikky <142957229+NadaMikky@users.noreply.github.com>
2025-04-29 13:28:28 -0400

    imported all the jsx files to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home';
+import Login from './login';
+import Navbar from './navbar';
+import Account from './account';
+import Listings from './listings';
+import CreateAccount from './createAccount';
 
 function App() {
   useEffect(() => {
@@ -14,6 +19,11 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/listings" element={<Listings />} />
+        <Route path="/create-account" element={<CreateAccount />} />
       </Routes>
     </Router>
   );
