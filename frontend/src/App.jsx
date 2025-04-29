2c235b054e08a0902ba13daf2832104a6f7c74d9
Nada Mikky <142957229+NadaMikky@users.noreply.github.com>
2025-04-29 13:32:41 -0400

    nvm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home';
-import Login from './login';
-import Navbar from './navbar';
-import Account from './account';
-import Listings from './listings';
-import CreateAccount from './createAccount';
+
 
 function App() {
   useEffect(() => {
@@ -19,11 +15,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/navbar" element={<Navbar />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/listings" element={<Listings />} />
-        <Route path="/create-account" element={<CreateAccount />} />
       </Routes>
     </Router>
   );
