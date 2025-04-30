022aec2c426f9cd652f59d15e9b9592ea86a7d51
Nada Mikky <142957229+NadaMikky@users.noreply.github.com>
2025-04-30 00:38:14 -0400

    will resume later

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import FirstPage from './firstPage';
 import Home from './home';
+import profile from './profile'; // Import Account component
 import Profile from './profile';
-import Listings from './listings';
-import Login from './login';
-import Navbar from './navbar';
 
 function App() {
   useEffect(() => {
@@ -18,21 +15,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<FirstPage />} /> {/* FirstPage without Navbar */}
-        <Route
-          path="*"
-          element={
-            <>
-              <Navbar /> {/* Navbar for all other pages */}
-              <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/listings" element={<Listings />} />
-              </Routes>
-            </>
-          }
-        />
-        <Route path="/login" element={<Login />} /> {/* Login page without Navbar */}
+        <Route path="/" element={<Home />} />
+        <Route path="/account" element={<Profile />} /> {/* Add route for Account */}
       </Routes>
     </Router>
   );
