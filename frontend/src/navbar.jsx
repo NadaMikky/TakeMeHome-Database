ad34e9a8a0588ecdad0afb8d35e8478ad47af5b5
Violet <yz0010@mix.wvu.edu>
2025-04-30 02:50:29 -0400

    Navbar fixed

--- frontend/src/navbar.jsx
+++ frontend/src/navbar.jsx
@@ -1,34 +1,55 @@
+// src/navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
-const Navbar = () => {
+// Import SVGs from src/icons/
+import wvuLogo      from './icons/wv_logo.svg';
+import listingsIcon from './icons/listings_white.svg';
+import accountIcon  from './icons/account_white.svg';
+import logoutIcon   from './icons/logout.svg';
+
+export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="navbar-logo">
-          <Link to="/">
-            <img src="icons/wv_logo.svg" alt="WVU Logo" className="navbar-logo-img" />
-            <h1 className="navbar-title">Take Me Home</h1>
-          </Link>
-        </div>
+        {/* .navbar-logo now on the Link itself */}
+        <Link to="/" className="navbar-logo">
+          <img
+            src={wvuLogo}
+            alt="WVU Logo"
+            className="navbar-logo-img"
+          />
+          <h1 className="navbar-title">Take Me Home</h1>
+        </Link>
+
         <div className="navbar-links">
           <Link to="/listings" className="nav-link">
-            <img src="icons/listings_white.svg" className="nav-icon" alt="Listings Icon" />
+            <img
+              src={listingsIcon}
+              alt="Listings Icon"
+              className="nav-icon"
+            />
             Listings
           </Link>
           <Link to="/account" className="nav-link">
-            <img src="icons/account_white.svg" className="nav-icon" alt="Account Icon" />
+            <img
+              src={accountIcon}
+              alt="Account Icon"
+              className="nav-icon"
+            />
             Account
           </Link>
           <Link to="/login" className="nav-link">
-            <img src="icons/logout.svg" className="nav-icon" alt="Logout Icon" />
+            <img
+              src={logoutIcon}
+              alt="Logout Icon"
+              className="nav-icon"
+            />
             Logout
           </Link>
         </div>
       </div>
     </nav>
   );
-};
-
-export default Navbar;
+}
