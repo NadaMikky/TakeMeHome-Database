7f8cf94798615fc5ba3d5287066c8a0da5fdf086
Brittany <bmorgan6@mix.wvu.edu>
2025-05-01 19:50:30 -0400

    Select query to verify success!

--- Avin.session.sql
+++ Avin.session.sql
@@ -56,6 +56,5 @@
 --     FOREIGN KEY (Passenger_ID) REFERENCES Passenger(Student_ID)
 -- );
 
--- INSERT INTO Users
--- VALUES('Jess', 'Remphrey','jer00027@mix.wvu.edu', 'password', '800727272', 'Sophomore');
+SELECT * FROM Users;
 
