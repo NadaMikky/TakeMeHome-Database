bbd87cd287217f8730298037290ef3cd7f4e533b
Brittany <bmorgan6@mix.wvu.edu>
2025-05-04 13:27:53 -0400

    Removed old database connection code

--- backend/server.js
+++ backend/server.js
@@ -33,40 +33,6 @@ app.post('/api/logout', (req, res) => {
     });
 });
 
-//database connection
-// const db = mysql.createConnection({
-//     host: 'mysql-edcf05-take-me-home.l.aivencloud.com',
-//     port: 15398,
-//     user: 'avnadmin',
-//     password: 'AVNS_CPA5OmNrKwBD2xMko_a',
-//     database: 'defaultdb',
-//     ssl: {
-//         rejectUnauthorized: false // optional, but recommended
-//     }
-// });
-// // subject to be removed 
-// function handleDisconnect() {
-//     db.connect(err => {
-//         if (err) {
-//             console.error('Error reconnecting to the database:', err);
-//            process.exit(1); 
-//         } else {
-//             console.log('Reconnected to the database.');
-//         }
-//     });
-
-//     db.on('error', err => {
-//         console.error('Database error:', err);
-//         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-//             handleDisconnect(); // Reconnect if the connection is lost
-//         } else {
-//             throw err;
-//         }
-//     });
-// }
-
-// handleDisconnect();
-
 // Database Queries
 
 app.get('/', (req, res) => {
